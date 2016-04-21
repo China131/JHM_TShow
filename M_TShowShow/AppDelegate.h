e49e2b5e295fb6cbe8e123640c373ddb657d048d
iOS163 <13162255392@163.com>
2016-04-21 15:00:30 +0800

    Initial Commit

---
+++ M_TShowShow/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  M_TShowShow
+//
+//  Created by cjatech-简豪 on 16/4/21.
+//  Copyright © 2016年 JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
